b54bb1a810cf67f69f9035d695685fcc72802256
Vasilis Ieropoulos <kirav96@gmail.com>
2017-01-26 22:22:19 +0200

    Update Update with email.bash

--- Update with email.bash
+++ Update with email.bash
@@ -6,7 +6,7 @@ myname=email@address.com              #insert your email so when proccess comple
 /bin/echo from: $myname >> $tmpfile
 /bin/echo to: $myname >> $tmpfile
 /bin/echo subject: Linux Update Status >> $tmpfile
-/bin/echo Hello there ROOT... this is an informational message: >> $tmpfile
+/bin/echo Hello there ROOT... this is an informational message: >> $tmpfile  #Displays a message 
 /bin/echo >> $tmpfile
 /bin/echo APT-GET Update was run: $( /bin/date +%c ) >> $tmpfile
 /bin/echo >> $tmpfile
