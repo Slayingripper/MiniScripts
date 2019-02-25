adc69a3fb2795db6c0d7d91267f632f679b05d61
Vasilis Ieropoulos <kirav96@gmail.com>
2019-02-25 09:37:33 +0000

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+echo
+
+echo -e "step 0: ${RED}Show Splash${NOCOLOR}"
+
+screenfetch
+
+echo
+
+echo -e "step 1: ${GREEN}pre-configuring packages${NOCOLOR}"
+sudo dpkg --configure -a
+
+echo
+
+echo -e "step 2: ${GREEN}fix and attempt to correct a system with broken dependencies${NOCOLOR}"
+sudo apt-get install -f
+
+echo
+
+echo -e "step 3: ${GREEN}update apt cache${NOCOLOR}"
+sudo apt-get update
+
+echo
+
+echo -e "step 4: ${GREEN}upgrade packages${NOCOLOR}"
+sudo apt-get upgrade
+
+echo
+
+echo -e "step 5: ${GREEN}distribution upgrade${NOCOLOR}"
+sudo apt-get dist-upgrade
+
+echo
+
+echo -e "step 6: ${GREEN}remove unused packages${NOCOLOR}"
+sudo apt-get --purge autoremove
+
+echo
+
+echo -e "step 7: ${GREEN}clean up${NOCOLOR}"
+sudo apt-get autoclean
+
+echo
+
+
